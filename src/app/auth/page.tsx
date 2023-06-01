9dee594e4de9c34dc3c7e081b82029cdc7b7eb55
unknown <marko.rodriguez@hotmail.es>
2023-06-01 18:02:38 -0500

    single quotes changed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link"
+import React from "react"
 
 const AuthLayout = () => {
   return (
-    <div className='w-full h-screen text-sm flex flex-col justify-center items-center'>
-      <div className='flex justify-between flex-col md:flex-row w-full h-full'>
-        <div className='w-8/12 bg-light-white hidden md:flex flex-col justify-center items-center'>
-          <img className='h-auto w-6/12' src="/assets/mowito.jpg" alt="login_img" />
+    <div className="w-full h-screen text-sm flex flex-col justify-center items-center">
+      <div className="flex justify-between flex-col md:flex-row w-full h-full">
+        <div className="w-8/12 bg-light-white hidden md:flex flex-col justify-center items-center">
+          <img className="h-auto w-6/12" src="/assets/mowito.jpg" alt="login_img" />
         </div>
-        <h2 className='w-full bg-dark-primary md:w-4/12 h-full flex flex-col items-center justify-center'>
-          <img className='h-20' src="/assets/mowa_logo.png" alt="logo_mowa" />
+        <h2 className="w-full bg-dark-primary md:w-4/12 h-full flex flex-col items-center justify-center">
+          <img className="h-20" src="/assets/mowa_logo.png" alt="logo_mowa" />
           <>
             <div className="w-full text-light-white my-4 max-w-xs">
               <form className="bg-light-dark shadow-md rounded px-8 pt-8 pb-6 mb-4">
@@ -29,8 +29,8 @@ const AuthLayout = () => {
 
             </div>
           </>
-          <button className='my-4 text-light-white bg-red-400 hover:opacity-90 px-10 py-2 rounded-lg'>Iniciar Sesión</button>
-          <Link className='text-sm underline underline-offset-8 hover:opacity-90 text-light-white' href="/#">¿No tienes cuenta?</Link>
+          <button className="my-4 text-light-white bg-red-400 hover:opacity-90 px-10 py-2 rounded-lg">Iniciar Sesión</button>
+          <Link className="text-sm underline underline-offset-8 hover:opacity-90 text-light-white" href="/#">¿No tienes cuenta?</Link>
         </h2>
       </div>
     </div>
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,22 +1,22 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link"
+import React from "react"
 
 const Page = () => {
   return (
-    <div className='w-full min-h-screen flex flex-col items-center justify-center bg-dark-primary radiant-bg'>
-      <form className='flex flex-col items-center bg-gray-400 p-10 rounded-lg shadow-md text-white justify-center'>
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-dark-primary radiant-bg">
+      <form className="flex flex-col items-center bg-gray-400 p-10 rounded-lg shadow-md text-white justify-center">
         <h1>Logo</h1>
-        <div className='mb-6 mt-4'>
+        <div className="mb-6 mt-4">
           <label htmlFor="">Usuario:</label> <br />
-          <input className='my-3'  type="text" />
+          <input className="my-3"  type="text" />
         </div>
         <div>
           <label htmlFor="">Contraseña:</label> <br />
-          <input className='my-3' type="text" />
+          <input className="my-3" type="text" />
         </div>
 
-        <button className='my-10 bg-gray-200 text-gray-700 font-semibold hover:opacity-80 transition-all w-full py-2 rounded-lg'>Iniciar Sesión</button>
-        <Link href="/auth/sign-up" className='text-sm underline underline-offset-8'>¿No tienes cuenta?</Link>
+        <button className="my-10 bg-gray-200 text-gray-700 font-semibold hover:opacity-80 transition-all w-full py-2 rounded-lg">Iniciar Sesión</button>
+        <Link href="/auth/sign-up" className="text-sm underline underline-offset-8">¿No tienes cuenta?</Link>
       </form>
     </div>
   )
