9c239ed5fa8d2d9c18cdbfb7c12643b13d02fd8f
Marko Rodriguez <marko.rodriguez@hotmail.es>
2023-06-08 23:15:14 -0500

    type cards added

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,30 +26,89 @@ const Admin = () => {
         setShow(isAdmin)
     }, [])
 
-    useEffect(()=>{
-        if(show){
+    useEffect(() => {
+        if (show) {
             getClassifiedMessages()
         }
     }, [show])
 
+
+
     const getClassifiedMessages = async () => {
-       const data = await axios.get("/api/answers/type?id=all")
-       console.log(data.data.values)
-       setDataMessages(data.data.values)
+        const data = await axios.get("/api/answers/type?id=all")
+        console.log(data.data.values)
+
+
+
+
+
+        setDataMessages(data.data.values)
     }
+
+
 
     return (
         <AuthHOC>{show ? (<main className="w-full h-screen min-h-screen bg-light-white">
             <Header title='Seguimiento del proyecto' />
 
-                <PieChart data={dataMessages} />
-            
+            <div className='md:w-8/12 bg-white mx-auto overflow-hidden flex flex-col md:flex-row w-10/12 h-auto rounded-lg shadow-md '>
 
-            {dataMessages.map((el:any, idx:number)=>{
-                return(<p key={idx}>
-                    {el.new_type} - {el.total}
-                </p>)
-            })}
+                <div className='md:w-4/12 w-full flex flex-col justify-center items-center h-full'>
+                    <div className='bg-sky-300 min-h-full mt-4 mb-8 relative shadow-md justify-around items-center flex w-10/12 mx-auto  py-5 rounded-lg'>
+                        <span className='rounded-full text-sky-300 px-2 py-2 bg-sky-500'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+
+                        </span>
+
+                        <div>
+                            <p className='text-sky-600 text-sm'> Total de mensajes calificados  </p>
+                            <p className='text-sky-800 text-center font-semibold text-xl'>{
+                                dataMessages.reduce((sum: any, currentValue: any) => {
+                                    const total = parseInt(currentValue.total);
+                                    return sum + total;
+                                }, 0)
+                            }</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='md:w-8/12 w-full bg-green-500 flex flex-col justify-center items-center h-full'>
+
+                    <div className='w-11/12 grid grid-cols-2 auto-rows-auto'>
+                        {dataMessages.map((el:any, idx:number) => {
+                            return (
+                                <div className='bg-sky-300 mt-4 mb-8 relative shadow-md justify-around items-center flex w-10/12 mx-auto  py-5 rounded-lg'>
+                                    <span className='rounded-full text-sky-300 px-2 py-2 bg-sky-500'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+
+                                    </span>
+
+                                    <div>
+                                        <p className='text-sky-600 text-sm'> {el.new_type} </p>
+                                        <p className='text-sky-800 text-center font-semibold text-xl'>
+                                            {el.total}
+                                        </p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
         </main>) : null}</AuthHOC>
     )
 }
--- src/components/app/admin/PieChart.tsx
+++ src/components/app/admin/PieChart.tsx
@@ -1,22 +1,55 @@
 import React from 'react'
-import { Chart } from "react-google-charts";
+
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 
 const PieChart = (props: any) => {
     const quantities = props.data
 
-    const data = [["Tipificación", "Cantidad"]]
+    // const data = [["Tipificación", "Cantidad"]]
 
+     const data = {
+        labels: quantities.map((el:any)=>el.new_type),
+        datasets: [
+          {
+            label: '# Respuestas',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.4)',
+              'rgba(54, 162, 235, 0.4)',
+              'rgba(255, 206, 86, 0.4)',
+              'rgba(75, 192, 192, 0.4)',
+              'rgba(153, 102, 255, 0.4)',
+              'rgba(255, 159, 64, 0.4)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+            hoverOffset: 10
+          },
+        ],
+      };
+
+
+      /*
     quantities.forEach((element: any) => {
         data.push([element.new_type, parseInt(element.total)])
-    });
+    
+    }); */
 
-    return (
-        <div >
-            <Chart chartType="PieChart" width={"50%"}
-                height={"400px"} data={data} />
-        </div>
-
-    )
+    return <Doughnut style={{width: "300px", height: "100px"}}  data={data} />
+    
+    
 }
 
 export default PieChart
